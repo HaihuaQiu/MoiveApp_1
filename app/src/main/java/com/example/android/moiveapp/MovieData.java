eed9e25cb0b00d365236fa2f7e0e171188917607
QHH <247354717@qq.com>
2017-01-13 15:27:00 +0800

    deal with many disadvantage which teachers tell.

--- app/src/main/java/com/example/android/moiveapp/MovieData.java
+++ app/src/main/java/com/example/android/moiveapp/MovieData.java
@@ -1,17 +1,56 @@
 package com.example.android.moiveapp;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by QHH on 2017/1/7.
  */
 
-public class MovieData implements Serializable {
+public class MovieData implements Parcelable {
+    public static final Parcelable.Creator CREATOR
+            = new Parcelable.Creator() {
+
+        // This simply calls our new constructor (typically private) and
+        // passes along the unmarshalled `Parcel`, and then returns the new object!
+        @Override
+        public MovieData createFromParcel(Parcel in) {
+            return new MovieData(in);
+        }
+
+        // We just need to copy this and change the type to match our class.
+        @Override
+        public MovieData[] newArray(int size) {
+            return new MovieData[size];
+        }
+    };
     public String name;
     public String resultStrs;
     public String year;
     public String score;
     public String overview;
 
+    public MovieData() {
 
+    }
+
+    private MovieData(Parcel in) {
+        String[] data = new String[5];
+        in.readStringArray(data);
+        this.name = data[0];
+        this.resultStrs = data[1];
+        this.year = data[2];
+        this.score = data[3];
+        this.overview = data[4];
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[]{this.name,
+                this.resultStrs, this.year, this.score, this.overview});
+    }
 }
--- app/src/main/java/com/example/android/moiveapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/moiveapp/SettingsActivity.java
@@ -5,29 +5,40 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by QHH on 2017/1/10.
  */
 
 public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add 'general' preferences, defined in the XML file
         // TODO: Add preferences from XML
         addPreferencesFromResource(R.xml.pref_general);
-        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
-        // updated when the preference changes.
-        // TODO: Add preferences
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_key)));
     }
 
-    /**
-     * Attaches a listener so the summary is always updated with the preference value.
-     * Also fires the listener once, to initialize the summary (so it shows up before the value
-     * is changed.)
-     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar_settings, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
     private void bindPreferenceSummaryToValue(Preference preference) {
         // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(this);
